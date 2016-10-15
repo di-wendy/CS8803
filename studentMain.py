2c4e4a601243062b08690e0cb7176fa333273348
Di Wang <jiankong3@gmail.com>
2016-10-15 10:08:38 -0400

    Instruction

---
+++ studentMain.py
@@ -0,0 +1,57 @@
+# ----------
+# Background
+# 
+# A robotics company named Trax has created a line of small self-driving robots 
+# designed to autonomously traverse desert environments in search of undiscovered
+# water deposits.
+#
+# A Traxbot looks like a small tank. Each one is about half a meter long and drives
+# on two continuous metal tracks. In order to maneuver itself, a Traxbot can do one
+# of two things: it can drive in a straight line or it can turn. So to make a 
+# right turn, A Traxbot will drive forward, stop, turn 90 degrees, then continue
+# driving straight.
+#
+# This series of questions involves the recovery of a rogue Traxbot. This bot has 
+# gotten lost somewhere in the desert and is now stuck driving in an almost-circle: it has
+# been repeatedly driving forward by some step size, stopping, turning a certain 
+# amount, and repeating this process... Luckily, the Traxbot is still sending all
+# of its sensor data back to headquarters.
+#
+# In this project, we will start with a simple version of this problem and 
+# gradually add complexity. By the end, you will have a fully articulated
+# plan for recovering the lost Traxbot.
+# 
+# ----------
+# Part One
+#
+# Let's start by thinking about circular motion (well, really it's polygon motion
+# that is close to circular motion). Assume that Traxbot lives on 
+# an (x, y) coordinate plane and (for now) is sending you PERFECTLY ACCURATE sensor 
+# measurements. 
+#
+# With a few measurements you should be able to figure out the step size and the 
+# turning angle that Traxbot is moving with.
+# With these two pieces of information, you should be able to 
+# write a function that can predict Traxbot's next location.
+#
+# You can use the robot class that is already written to make your life easier. 
+# You should re-familiarize yourself with this class, since some of the details
+# have changed. 
+#
+# ----------
+# YOUR JOB
+#
+# Complete the estimate_next_pos function. You will probably want to use
+# the OTHER variable to keep track of information about the runaway robot.
+#
+# ----------
+# GRADING
+# 
+# We will make repeated calls to your estimate_next_pos function. After
+# each call, we will compare your estimated position to the robot's true
+# position. As soon as you are within 0.01 stepsizes of the true position,
+# you will be marked correct and we will tell you how many steps it took
+# before your function successfully located the target bot.
+
+# These import steps give you access to libraries which you may (or may
+# not) want to use.
